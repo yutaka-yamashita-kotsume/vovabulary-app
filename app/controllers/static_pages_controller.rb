a5f9f1fcc7c6c58c44b13716d971d9eb242b5322
山下豊 <yamashitayutaka@yamashitayutakanoMacBook-Air.local>
2026-02-15 20:06:33 +0900

    プライバシポリシーの設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  # ログインしていなくても、terms と privacy だけは見れるように許可する
+  allow_unauthenticated_access only: [:terms, :privacy]
+  
   def terms
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
   get "terms", to: "static_pages#terms", as: :terms     # as: :terms を追加
   get "privacy", to: "static_pages#privacy", as: :privacy # as: :privacy を追加
+  
+  root "words#home"
+
   get "static_pages/terms"
   get "static_pages/privacy"
   get "registrations/new"
   get "registrations/create"
-  root "words#home"
 
+  # クイズ関連
   get "quiz", to: "quiz#index"
   get "quiz/reset", to: "quiz#reset"
   post "quiz/record_answer", to: "quiz#record_answer"
+
+  # 認証・登録
   resource :session
   resources :passwords, param: :token
   resources :registrations, only: [:new, :create]
+
+  # 単語帳
   resources :words
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
